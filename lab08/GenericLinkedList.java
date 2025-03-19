3107775413f2c9b852ff24b18fdfb48b5a5eb773
Trung Nguyen <nttrungmt@gmail.com>
2025-03-19 11:42:07 -0500

    Update GenericLinkedList.java

--- lab08/GenericLinkedList.java
+++ lab08/GenericLinkedList.java
@@ -1,45 +1,45 @@
 package lab08;
 import java.util.ArrayList;
 public class GenericLinkedList<E> {
-	private Node<T> head;  // Head of the linked list
+    private Node<T> head;  // Head of the linked list
 
     // Constructor method
     public GenericLinkedList() {
         head = null; //head is null and the list is empty at the beginning
     }
     
-	/* Return the size of the linked list */
-	public int size() {
-		// TODO Add code below
-	}
+    /* Return the size of the linked list */
+    public int size() {
+        // TODO Add code below
+    }
 
-	/* Add a new element to the end of the linked list */
-	public void add(E obj) {
-		// TODO Add code below
-	}
+    /* Add a new element to the end of the linked list */
+    public void add(E obj) {
+        // TODO Add code below
+    }
 
-	/* Remove and return the element at the given index in the linked list */
-	public E remove(int index) {
-		// TODO Add code below
-	}
+    /* Remove and return the element at the given index in the linked list */
+    public E remove(int index) {
+        // TODO Add code below
+    }
 
-	/* Remove true if the given obj is removed from the linked list */
-	public boolean remove(E obj) {
-		// TODO Add code below
-	}
+    /* Remove true if the given obj is removed from the linked list */
+    public boolean remove(E obj) {
+        // TODO Add code below
+    }
 
-	/* Return true if the linked list is empty */
-	public boolean isEmpty() {
-		// TODO Add code below
-	}
-	
-	/* Return the string representation of the linked list */
-	@Override
-	public String toString() {
-		// TODO Add code below
-	}
+    /* Return true if the linked list is empty */
+    public boolean isEmpty() {
+        // TODO Add code below
+    }
+    
+    /* Return the string representation of the linked list */
+    @Override
+    public String toString() {
+        // TODO Add code below
+    }
 
-	public static void main(String[] args) {
-		// TODO Add code below
-	}
+    public static void main(String[] args) {
+        // TODO Add code below
+    }
 }
