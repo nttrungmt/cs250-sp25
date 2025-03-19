f45b10ccf67b0a9b1264cc769fc3be426b26ee6c
Trung Nguyen <nttrungmt@gmail.com>
2025-03-19 11:22:51 -0500

    Create Prog1.java

---
+++ lab08/Prog1.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Prog1 {
+    /**
+      * The generic method to print an array of a generic type
+      * @param arrObj: the array of the generic type E
+      * @return: none
+      */
+    private static <E> void printArray(E[] arrObj) {
+        //TODO: add code below
+    }
+    
+    private static <T extends Number> double max(ArrayList<T> list) {
+         //TODO: add code below
+    }
+	
+    public static void main(String[] args) {
+        //TODO add code below
+    }
+}
+
